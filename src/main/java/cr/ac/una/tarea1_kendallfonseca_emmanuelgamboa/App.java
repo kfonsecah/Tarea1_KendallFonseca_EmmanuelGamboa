aceda508a4457bc7cb2a6d3013e0bea72cb53912
Kendall Fonseca <132479325+kfonsecah@users.noreply.github.com>
2024-03-13 01:07:32 -0600

    Initial Commit

---
+++ src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/App.java
@@ -0,0 +1,32 @@
+package cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa;
+
+import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.util.FlowController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FlowController.getInstance().InitializeFlow(stage, null);
+        FlowController.getInstance().goMain();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
