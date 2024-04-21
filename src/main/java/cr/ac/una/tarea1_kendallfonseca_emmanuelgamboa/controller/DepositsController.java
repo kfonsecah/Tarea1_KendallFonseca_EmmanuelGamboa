b4b19305ffa8211d4e8723e31853b9f1e757b87b
Kendall Fonseca Hidalgo <kendall.fonseca.hidalgo@est.una.ac.cr>
2024-04-21 15:40:18 -0600

    kn

--- src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/DepositsController.java
+++ src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/DepositsController.java
@@ -8,17 +8,23 @@ import java.net.URL;
 import java.security.cert.PolicyNode;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Function;
 
 import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.model.Deposits;
 import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.util.AppContext;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXCheckListView;
+import io.github.palexdev.materialfx.controls.MFXTableColumn;
+import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXCheckListCell;
+import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -34,16 +40,47 @@ public class DepositsController extends Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         loadDeposits();
+        initializeTableView();
         // TODO
     }
 
     private void loadDeposits() {
-
         ObservableList<Deposits> deposits = AppContext.getInstance().getDeposits();
 
+        pendingDeposits.setItems(deposits);
 
+
+    }
+
+
+
+    private void initializeTableView() {
+        // Initialize columns
+        MFXTableColumn<Deposits> accountNumberColumn = new MFXTableColumn<>("Account Number");
+        MFXTableColumn<Deposits> accountTypeColumn = new MFXTableColumn<>("Account Type");
+
+        MFXTableColumn<Deposits> customerNameColumn = new MFXTableColumn<>("Customer Name");
+        MFXTableColumn<Deposits> amountColumn = new MFXTableColumn<>("Amount");
+
+        // Set cell value factories
+
+
+
+
+
+
+        // Set items to the table view
+        ObservableList<Deposits> deposits = AppContext.getInstance().getDeposits();
         pendingDeposits.setItems(deposits);
     }
+
+
+
+
+
+
+
+
 
     // Method to retrieve deposit
 
@@ -60,7 +97,7 @@ public class DepositsController extends Controller implements Initializable {
     private MFXButton acceptDeposits;
 
     @FXML
-    private MFXCheckListView<Deposits> pendingDeposits;
+    private MFXTableView<Deposits> pendingDeposits;
 
     @FXML
     private MFXButton removeDeposits;
