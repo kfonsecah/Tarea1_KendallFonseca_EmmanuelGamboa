a5a70cee9828346e148165009d36a76fed2a9cd3
Kendall Fonseca Hidalgo <kendall.fonseca.hidalgo@est.una.ac.cr>
2024-04-21 01:39:30 -0600

    dad

--- src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/AssociateMaintenanceController.java
+++ src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/AssociateMaintenanceController.java
@@ -268,7 +268,7 @@ public class AssociateMaintenanceController extends Controller implements Initia
                 );
 
                 try {
-                   selectedUser.addAssociatedToJsonFile(associatedData);
+                   AppContext.addAssociatedToJsonFile(associatedData);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
