5f3c255ea9a850dc74530795631ece784100b371
EmmanuelGamboaRetana <luis.gamboa.retana@est.una.ac.cr>
2024-04-21 02:34:20 -0600

    updates

--- src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/WithdrawDepositsController.java
+++ src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/WithdrawDepositsController.java
@@ -6,9 +6,12 @@ package cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import io.github.palexdev.materialfx.controls.MFXSpinner;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -25,6 +28,46 @@ public class WithdrawDepositsController extends Controller implements Initializa
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
+
+    @FXML
+    private MFXSpinner<?> addFiftyC;
+
+    @FXML
+    private MFXSpinner<?> addFiveBill;
+
+    @FXML
+    private MFXSpinner<?> addFiveC;
+
+    @FXML
+    private MFXSpinner<?> addFiveHundredC;
+
+    @FXML
+    private MFXSpinner<?> addOneBill;
+
+    @FXML
+    private MFXSpinner<?> addOneHundredC;
+
+    @FXML
+    private MFXSpinner<?> addTenBill;
+
+    @FXML
+    private MFXSpinner<?> addTenC;
+
+    @FXML
+    private MFXSpinner<?> addTwentyBill;
+
+    @FXML
+    private MFXSpinner<?> addTwentyC;
+
+    @FXML
+    private MFXSpinner<?> addTwoBill;
+
+    @FXML
+    private TableView<?> userDepositsList;
+
+    @FXML
+    private TableView<?> userFolioList;
+
     @FXML
     private AnchorPane root;
 
