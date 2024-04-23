3ce9a8c3ac50eced99a03a49a73069f3579b4b77
Kendall Fonseca <132479325+kfonsecah@users.noreply.github.com>
2024-04-22 21:28:13 -0600

    Update MovementsController.java

---
+++ src/main/java/cr/ac/una/tarea1_kendallfonseca_emmanuelgamboa/controller/MovementsController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.model.Account;
+import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.model.Associated;
+import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.model.Deposits;
+import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.util.AccountUser;
+import cr.ac.una.tarea1_kendallfonseca_emmanuelgamboa.util.Mensaje;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author gambo
+ */
+public class MovementsController extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    @Override
+    public void initialize() {
+    }
+}
